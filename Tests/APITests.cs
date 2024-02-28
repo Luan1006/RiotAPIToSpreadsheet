8a5f7d3d25cbe390b739230859cf4912c3e87d29
Luan1006 <thenoobsisbest@gmail.com>
2024-02-28 16:25:08 +0100

    Created AccountV1ClassCanBeInstantiated in APITests.cs

--- Tests/APITests.cs
+++ Tests/APITests.cs
@@ -1,2 +1,15 @@
 using Xunit;
 using Luan1006.RiotAPI.Library;
+
+public class APITests
+{
+    [Fact]
+    public void AccountV1ClassCanBeInstantiated()
+    {
+        // Act
+        AccountV1 accountV1 = new AccountV1();
+
+        // Assert
+        Assert.NotNull(accountV1);
+    }
+}
