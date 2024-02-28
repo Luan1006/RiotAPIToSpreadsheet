a4bbb050de4b10e57ca8675f640464c14a7e11b9
Luan1006 <thenoobsisbest@gmail.com>
2024-02-28 16:31:05 +0100

    Created puuid property in User class

--- User.cs
+++ User.cs
@@ -2,6 +2,7 @@ namespace Luan1006.RiotAPI
 {
     public class User
     {
+        public string puuid { get; set; }
         public string gameName { get; set; }
         public string tagLine { get; set; }
     }
