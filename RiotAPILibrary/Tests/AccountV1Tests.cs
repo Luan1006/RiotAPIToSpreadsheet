0e14f9116e8b3bd905bdc3e219816d375990bd30
Luan1006 <thenoobsisbest@gmail.com>
2024-03-01 18:25:15 +0100

    Removed test from AccountV1Tests

--- RiotAPILibrary/Tests/AccountV1Tests.cs
+++ RiotAPILibrary/Tests/AccountV1Tests.cs
@@ -14,21 +14,5 @@ namespace Luan1006.RiotAPI.Library.Tests
             // Assert
             Assert.NotNull(accountV1);
         }
-
-        [Fact]
-        public void GetAccountByPuuidMethodReturnsAccountDto()
-        {
-            // Arrange
-            AccountV1 accountV1 = new AccountV1();
-            string puuid = "0";
-            string apiKey = APIKey.Key;
-
-            // Act
-            AccountDto account = accountV1.GetAccountByPuuid(puuid, apiKey).Result;
-
-            // Assert
-            Assert.NotNull(account);
-        }
-
     }
 }
